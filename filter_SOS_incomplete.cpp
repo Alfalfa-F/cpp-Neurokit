1441968d8b786cb1c472c99d297193299c2eed33
Alfalfa <135498915+Alfalfa-F@users.noreply.github.com>
2023-08-09 04:21:03 +0800

    Update filter_SOS_incomplete.cpp

--- filter_SOS_incomplete.cpp
+++ filter_SOS_incomplete.cpp
@@ -1,45 +1,60 @@
-#include <iostream>
-#include <stdio.h>
-#include "signal_filter.h"
-#include <Eigen/Dense>
-#include <Iir.h>
-Eigen::MatrixXd filter_elgendi(Eigen::MatrixXd signal){
-
-// 原scipy使用的滤波器
-return signal;
-}
-
-Eigen::MatrixXd filter_nabian2018(Eigen::MatrixXd signal){
-    //原scipy使用的滤波器
-    return signal;
-}
+#include<iostream>
+#include<Eigen/Dense>
+#include "D:\\NeuroKit\\iir1-1.9.4\\Iir.h"
+#include "D:\\NeuroKit\\iir1-1.9.4\\iir\\Custom.h"
 
 
-Eigen::MatrixXd filter_SOS(Eigen::MatrixXd signal, int sampling_rate){
-    // const double[][6] SOS = getSOS()
-  
-    length = signal.cols(); //拿到signal的大小
+class PPGFilter{
+    private:
+        Eigen::MatrixXd data;
     
-    const double SOS[][6];   //未确定SOS的大小
-    const int nSOS = sizeof(SOS) / sizeof(SOS[0]);
-    try {
-        Iir::Custom::SOSCascade<nSOS> cust(SOS);;
+    public:
+    
+    PPGFilter(){}
+
+    PPGFilter(Eigen::MatrixXd signal){
+        data = signal;
+        int n = signal.array().isNaN().sum();
+        if(n > 0){
+            std::cerr <<  "There are " << n << " missing data points in your signal."
+                    " Filling missing values by using the forward filling method." << std::endl;
+            data = ppg_clean_missing(signal);
+        };
     }
-    catch (const std::exception& e) {
-        std::cerr << "Wrong parameters" << std::endl;
-        return;
-    }
-  double filter_signal[length];  //创建过滤后的信号array
-  double* signal_array= signal.data();
     
-    //过滤signal array
-    for (int i = 0; i < length; i++)
-	{
-		filter_signal[i] = cust.filter(signal_array[i]);
     
-	}
-  
-  Eigen::Map<Eigen::MatrixXd> res(filter_signal,1,length) //从array还原回MatrixXd
+    template <int nSOS>
+        Eigen::MatrixXd SOSFilter(const double (&SOS_Parameter)[nSOS][6]){
+            int length = data.cols();    //拿到signal的大小
+        
+        // const int nSOS = sizeof(SOS_Parameter) / sizeof(SOS_Parameter[0]);
+        try {
+            Iir::Custom::SOSCascade<nSOS> cust(SOS_Parameter);
 
-return res;
-}
+             double filter_signal[length];    //创建过滤后的信号array
+             double *signal_array= data.data(); //过滤signal array
+            for (int i = 0; i < length; i++)
+	        {
+		        filter_signal[i] = cust.filter(signal_array[i]);
+	        }
+  
+            Eigen::Map<Eigen::MatrixXd> res(filter_signal,1,length);    //从array还原回MatrixXd
+            return res;
+        }
+
+        catch (const std::exception& e) {
+            std::cerr << "Wrong parameters" << std::endl;
+            return Eigen::MatrixXd();
+        }
+    }
+
+    Eigen::MatrixXd PPGCleanMissing(Eigen::MatrixXd ppg_signal){
+        int r = ppg_signal.cols();// 经过as_vector的处理，ppg_signal 应该已经变成一个一维的数组了
+        for(int i = 0; i < r; i++ ){
+            if(i >= 1 && std::isnan(ppg_signal(0,i))){
+                ppg_signal(0,i) = ppg_signal(0,i-1);  //原python的方法也不能保证第一个值为NaN的时候进行填充
+            };
+        }
+        return ppg_signal;
+    }
+};
